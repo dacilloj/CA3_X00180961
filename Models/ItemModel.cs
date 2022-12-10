5fcf50144c3f82428302617fae647f5b25a4773e
dacillo <johncarlodacillo123@gmail.com>
2022-12-10 19:02:31 +0000

    added item model and product model

--- Models/ItemModel.cs
+++ Models/ItemModel.cs
@@ -1,7 +1,26 @@
-﻿namespace Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
 {
     public class ItemModel
     {
+        //Primary Attributes
+        [Required]
+        [Key]
+        [Display(Name = "ID")]
+        public int ItemId { get; set; }
 
+        [Required]
+        [Display (Name = "Item")]
+        public string ItemName { get; set; } = "";
+
+
+        public string Description { get; set; } = "";
+
+        public string ItemType { get; set;  } = "";
+
+
+        //foreign attributes
+        
     }
 }
---
+++ Models/ProductModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ProductModel
+    {
+        [Required]
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public double ProductPrice { get; set; }
+
+        //foreign relationships
+        [Required]
+        public int ShopID { get; set; }
+
+        [Required]
+        public int ItemID { get; set; }
+
+
+    }
+}
