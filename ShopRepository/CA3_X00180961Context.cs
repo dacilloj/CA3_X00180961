ca82fa4d6e4bde18e6cdffe83f1308254b141682
dacillo <johncarlodacillo123@gmail.com>
2022-12-10 22:01:21 +0000

    added project reference

---
+++ ShopRepository/CA3_X00180961Context.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace CA3_X00180961.Data
+{
+    public class CA3_X00180961Context : DbContext
+    {
+        public CA3_X00180961Context (DbContextOptions<CA3_X00180961Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Models.ProductFromShopAModel> ProductModel { get; set; } = default!;
+
+        public DbSet<Models.ItemModel> ItemModel { get; set; } = default!;
+
+        public DbSet<Models.ProductFromShopBModel> ProductFromShopBModel { get; set; } = default!;
+
+
+        public DbSet<Models.ShopModel> ShopModel { get; set; } = default!;
+
+    }
+}
