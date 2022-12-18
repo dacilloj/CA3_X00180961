bd600da034f8488ca69eb34152ac3cb00dd8fcae
dacillo <johncarlodacillo123@gmail.com>
2022-12-18 02:34:36 +0000

    unit test

--- ShopTest/IShopRepoTest.cs
+++ ShopTest/IShopRepoTest.cs
@@ -1,4 +1,5 @@
 using CA3_X00180961.Controllers;
+using CA3_X00180961.ShopRepository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,41 +15,35 @@ namespace ShopTest
     [TestClass]
     public class IShopRepoTest
     {
-        Mock<IShopRepo> _shopRepo = new Mock<IShopRepo>();
-
-        [TestMethod]
-        public void GetAllItemsTest()
+        public static object GetMock()
         {
-            var expected = new List<ItemModel>(1)
-            {
-                new ItemModel() {ItemId= 1, ItemName = "ItemName", ItemType = "ItemType"}
-             };
-
-
+            var mock =  new Mock<IShopRepo>();
+            mock.Setup(m => m.GetItemModels()).Returns(() => new Mock<IEnumerable<ItemModel>>().Object);
             
-            _shopRepo.Setup(x => x.GetItemModels()).Returns(() => expected);
-            var controller = new ItemsController(_shopRepo.Object);
-         
-            //ACT
-            var response = controller.GetItemModel();
-          
-
-            Assert.AreEqual(expected.GetType(), response.GetType());
-           
-            
+            return mock;
         }
 
+
         [TestMethod]
-        public void GetReturnsNotFound()
+        public void TestRepo()
         {
-            // Arrange
-            var controller = new ItemsController(_shopRepo.Object);
+           var repoMock = GetMock();
+            Assert.IsNotNull(repoMock);
 
-            // Act
-            //IHttpActionResult actionResult = controller.Delete(10);
 
-            // Assert
-            //Assert.IsInstanceOfType(actionResult, typeof(NotFoundResult));
+        }
+
+
+        private List<ItemModel> GetTestItems()
+        {
+            var testProducts = new List<ItemModel>();
+            testProducts.Add(new ItemModel { ItemId = 1, ItemName = "Demo1", ItemType = "Type1" });
+            testProducts.Add(new ItemModel { ItemId = 2, ItemName = "Demo2", ItemType = "Type2" });
+            testProducts.Add(new ItemModel { ItemId = 3, ItemName = "Demo3", ItemType = "Type3" });
+            testProducts.Add(new ItemModel { ItemId = 4, ItemName = "Demo4", ItemType = "Type4" });
+
+            return testProducts;
         }
     }
+
 }
